4d7a77776bd6bbb71079609615197d0bad655b75
Dave <david.chinweike@sastrify.com>
2024-10-19 12:03:37 +0100

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { auth, BASE_PATH } from "@/auth";
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|manifest.json).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|manifest.json|ios/*.png|android/*.png).*)"],
 };
 
 export default auth((req) => {
