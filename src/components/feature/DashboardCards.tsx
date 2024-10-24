a2bcca4cedd1969e5f13131532db171d45bdeb8a
Emmanuel <emmanuelugochukwu2000@gmail.com>
2024-10-24 19:21:10 +0100

    Your commit message

--- src/components/feature/DashboardCards.tsx
+++ src/components/feature/DashboardCards.tsx
@@ -11,7 +11,7 @@ export function DashboardCards() {
 
   const { push } = useRouter();
   const { data, isLoading, bank, isLoadingBank } = useContext(AssetContext);
-  
+
   const theme = useTheme();  // Use MUI theme to adapt light/dark mode
 
   return (
@@ -23,7 +23,8 @@ export function DashboardCards() {
       padding: { sm: DESKTOP_CONTAINER_PADDING, xs: MOBILE_CONTAINER_PADDING },
       mt: { xs: 2 },
       bgcolor: theme.palette.background.default,  // Dynamically set the background color
-      color: theme.palette.text.primary  // Text color adapts to the theme mode
+      color: theme.palette.text.primary,  // Text color adapts to the theme mode
+      transition: 'background-color 0.3s, color 0.3s'  // Add smooth transition between light and dark mode
     }}>
       {
         isLoading && isLoadingBank ? (
